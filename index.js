9c04573dcc8926a5dcef90fcac4189d0c50438ae
Anastasiia Vasilchenko <nastyavasilshenko@mail.ru>
2025-05-30 10:18:51 +0300

    merge feature/user-access in main

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 console.log('Hello World');
 
 const userAge = 35;
-If (userAge >= 18) {
+if (userAge >= 18) {
   console.log('Вы совершеннолетний');
 } else {
   console.log('Вы НЕ совершеннолетний');
